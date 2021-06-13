be944afce9a962c4ff5cb319d15dbf349d7ac488
SpiderMath <41898282+github-actions[bot]@users.noreply.github.com>
2021-06-13 19:38:15 +0530

    Added Programming Quotes and removed the sample dataset

--- Source/Actions.js
+++ Source/Actions.js
@@ -1,11 +1,11 @@
-const core = require('@actions/core');
-const { Toolkit } = require('actions-toolkit');
-const { readFileSync, writeFileSync } = require('fs');
-const quotes = require('./Quotes.json');
-const { stripIndents } = require('common-tags');
-const { spawn } = require('child_process');
+const core = require("@actions/core");
+const { Toolkit } = require("actions-toolkit");
+const { readFileSync, writeFileSync } = require("fs");
+const quotes = require("./Quotes.json");
+const { stripIndents } = require("common-tags");
+const { spawn } = require("child_process");
 
-let QUOTE_FONT_SIZE = core.getInput('QUOTE_FONT_SIZE');
+let QUOTE_FONT_SIZE = core.getInput("QUOTE_FONT_SIZE");
 
 function getRandomQuote() {
 	return quotes[
@@ -22,15 +22,15 @@ async function execute(cmd, args = []) {
 	new Promise((resolve, reject) => {
 		const child = spawn(cmd, args);
 
-		child.stderr.on('data', (data) => {
+		child.stderr.on("data", (data) => {
 			return reject(data);
 		});
 
-		child.stdout.on('data', (data) => {
+		child.stdout.on("data", (data) => {
 			console.log(data);
 		});
 
-		child.on('data', (data) => console.log(data));
+		child.on("data", (data) => console.log(data));
 
 		resolve();
 	});
@@ -38,36 +38,36 @@ async function execute(cmd, args = []) {
 
 Toolkit
 	.run(async (tools) => {
-		if(isNaN(QUOTE_FONT_SIZE)) return tools.exit.failure('QUOTE_FONT_SIZE is not a number!');
+		if(isNaN(QUOTE_FONT_SIZE)) return tools.exit.failure("QUOTE_FONT_SIZE is not a number!");
 		QUOTE_FONT_SIZE = Math.floor(Number(QUOTE_FONT_SIZE));
 
-		if(QUOTE_FONT_SIZE > 6 || QUOTE_FONT_SIZE < 1) return tools.exit.failure('QUOTE_FONT_SIZE is out of scope, the value has to be an integer between 1 & 6');
+		if(QUOTE_FONT_SIZE > 6 || QUOTE_FONT_SIZE < 1) return tools.exit.failure("QUOTE_FONT_SIZE is out of scope, the value has to be an integer between 1 & 6");
 
-		const readmeContent = readFileSync('./README.md', 'utf-8').split('\n');
+		const readmeContent = readFileSync("./README.md", "utf-8").split("\n");
 
-		const startIndex = readmeContent.findIndex(content => content.trim() === '<!--QUOTE-BOT:start-->');
-		const endIndex = readmeContent.findIndex(content => content.trim() === '<!--QUOTE-BOT:end-->');
+		const startIndex = readmeContent.findIndex(content => content.trim() === "<!--QUOTE-BOT:start-->");
+		const endIndex = readmeContent.findIndex(content => content.trim() === "<!--QUOTE-BOT:end-->");
 
-		if(startIndex === -1) return tools.exit.failure('Couldn\'t find the <!--QUOTE-BOT:start--> comment! Exiting the process...');
-		if(endIndex === -1) return tools.exit.failure('Couldn\'t find the <!--QUOTE-BOT:end--> comment! Exiting the process...');
+		if(startIndex === -1) return tools.exit.failure("Couldn't find the <!--QUOTE-BOT:start--> comment! Exiting the process...");
+		if(endIndex === -1) return tools.exit.failure("Couldn't find the <!--QUOTE-BOT:end--> comment! Exiting the process...");
 
 		if(startIndex !== endIndex) readmeContent.splice(startIndex + 1, (endIndex - startIndex) - 1);
 
 		const quote = getRandomQuote();
 		const string = stripIndents`
-			${'#'.repeat(QUOTE_FONT_SIZE)} <i>${quote.text}</i><br>
-			${'#'.repeat(QUOTE_FONT_SIZE)} - <b>${quote.author}</b><br>		
+			${"#".repeat(QUOTE_FONT_SIZE)} <i>${quote.text}</i><br>
+			${"#".repeat(QUOTE_FONT_SIZE)} - <b>${quote.author}</b><br>		
 		`;
 
 		readmeContent.splice(startIndex + 1, 0, string);
 
-		writeFileSync('./README.md', readmeContent.join('\n').toString());
+		writeFileSync("./README.md", readmeContent.join("\n").toString());
 
-		await execute('git', ['config', '--local', 'user.email', '41898282+github-actions[bot]@users.noreply.github.com']);
-		await execute('git', ['config', '--local', 'user.name', 'Trial Bot']);
-		await execute('git', ['add', '-A']);
-		await execute('git', ['commit', '-m', 'Tried something new?']);
-		await execute('git', ['push']);
+		await execute("git", ["config", "--local", "user.email", "41898282+github-actions[bot]@users.noreply.github.com"]);
+		await execute("git", ["config", "--local", "user.name", "Trial Bot"]);
+		await execute("git", ["add", "-A"]);
+		await execute("git", ["commit", "-m", "Tried something new?"]);
+		await execute("git", ["push"]);
 	}, {
-		events: ['schedule', 'workflow_dispatch'],
+		events: ["schedule", "workflow_dispatch"]
 	});
--- dist/index.js
+++ dist/index.js
@@ -7371,7 +7371,7 @@ const quotes = __webpack_require__(418);
 const { stripIndents } = __webpack_require__(971);
 const { spawn } = __webpack_require__(129);
 
-let QUOTE_FONT_SIZE = core.getInput('QUOTE_FONT_SIZE');
+let QUOTE_FONT_SIZE = core.getInput("QUOTE_FONT_SIZE");
 
 function getRandomQuote() {
 	return quotes[
@@ -7388,15 +7388,15 @@ async function execute(cmd, args = []) {
 	new Promise((resolve, reject) => {
 		const child = spawn(cmd, args);
 
-		child.stderr.on('data', (data) => {
+		child.stderr.on("data", (data) => {
 			return reject(data);
 		});
 
-		child.stdout.on('data', (data) => {
+		child.stdout.on("data", (data) => {
 			console.log(data);
 		});
 
-		child.on('data', (data) => console.log(data));
+		child.on("data", (data) => console.log(data));
 
 		resolve();
 	});
@@ -7404,38 +7404,38 @@ async function execute(cmd, args = []) {
 
 Toolkit
 	.run(async (tools) => {
-		if(isNaN(QUOTE_FONT_SIZE)) return tools.exit.failure('QUOTE_FONT_SIZE is not a number!');
+		if(isNaN(QUOTE_FONT_SIZE)) return tools.exit.failure("QUOTE_FONT_SIZE is not a number!");
 		QUOTE_FONT_SIZE = Math.floor(Number(QUOTE_FONT_SIZE));
 
-		if(QUOTE_FONT_SIZE > 6 || QUOTE_FONT_SIZE < 1) return tools.exit.failure('QUOTE_FONT_SIZE is out of scope, the value has to be an integer between 1 & 6');
+		if(QUOTE_FONT_SIZE > 6 || QUOTE_FONT_SIZE < 1) return tools.exit.failure("QUOTE_FONT_SIZE is out of scope, the value has to be an integer between 1 & 6");
 
-		const readmeContent = readFileSync('./README.md', 'utf-8').split('\n');
+		const readmeContent = readFileSync("./README.md", "utf-8").split("\n");
 
-		const startIndex = readmeContent.findIndex(content => content.trim() === '<!--QUOTE-BOT:start-->');
-		const endIndex = readmeContent.findIndex(content => content.trim() === '<!--QUOTE-BOT:end-->');
+		const startIndex = readmeContent.findIndex(content => content.trim() === "<!--QUOTE-BOT:start-->");
+		const endIndex = readmeContent.findIndex(content => content.trim() === "<!--QUOTE-BOT:end-->");
 
-		if(startIndex === -1) return tools.exit.failure('Couldn\'t find the <!--QUOTE-BOT:start--> comment! Exiting the process...');
-		if(endIndex === -1) return tools.exit.failure('Couldn\'t find the <!--QUOTE-BOT:end--> comment! Exiting the process...');
+		if(startIndex === -1) return tools.exit.failure("Couldn't find the <!--QUOTE-BOT:start--> comment! Exiting the process...");
+		if(endIndex === -1) return tools.exit.failure("Couldn't find the <!--QUOTE-BOT:end--> comment! Exiting the process...");
 
 		if(startIndex !== endIndex) readmeContent.splice(startIndex + 1, (endIndex - startIndex) - 1);
 
 		const quote = getRandomQuote();
 		const string = stripIndents`
-			${'#'.repeat(QUOTE_FONT_SIZE)} <i>${quote.text}</i><br>
-			${'#'.repeat(QUOTE_FONT_SIZE)} - <b>${quote.author}</b><br>		
+			${"#".repeat(QUOTE_FONT_SIZE)} <i>${quote.text}</i><br>
+			${"#".repeat(QUOTE_FONT_SIZE)} - <b>${quote.author}</b><br>		
 		`;
 
 		readmeContent.splice(startIndex + 1, 0, string);
 
-		writeFileSync('./README.md', readmeContent.join('\n').toString());
+		writeFileSync("./README.md", readmeContent.join("\n").toString());
 
-		await execute('git', ['config', '--local', 'user.email', '41898282+github-actions[bot]@users.noreply.github.com']);
-		await execute('git', ['config', '--local', 'user.name', 'Trial Bot']);
-		await execute('git', ['add', '-A']);
-		await execute('git', ['commit', '-m', 'Tried something new?']);
-		await execute('git', ['push']);
+		await execute("git", ["config", "--local", "user.email", "41898282+github-actions[bot]@users.noreply.github.com"]);
+		await execute("git", ["config", "--local", "user.name", "Trial Bot"]);
+		await execute("git", ["add", "-A"]);
+		await execute("git", ["commit", "-m", "Tried something new?"]);
+		await execute("git", ["push"]);
 	}, {
-		events: ['schedule', 'workflow_dispatch'],
+		events: ["schedule", "workflow_dispatch"]
 	});
 
 /***/ }),
